50619c0ef431c8119000cf6a878b5299ff70c535
Jhony Souza <jhonatansouzameza100@gmail.com>
2022-10-10 02:19:57 -0300

    Upload tutorials js & bootstrap

---
+++ JavaScript/script.js
@@ -0,0 +1,6 @@
+/*$(document).ready(function () {
+    $('h1').addClass("color-red");
+    });
+
+var time = new Date();
+console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());*/
